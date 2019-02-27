48721931b51dff327c24e039a3fc0c5c4dd04f11
Alejandro <jandrito97@gmail.com>
2019-02-27 17:55:47 +0100

    Setters en vehiculo.h

--- LabAssignment2ComputerArchitecture/vehiculo.h
+++ LabAssignment2ComputerArchitecture/vehiculo.h
@@ -31,8 +31,20 @@ public:
 
     //setters
     //Main Configuration
-    void setNombre();
+    void setNombre(string Nombre);
+    void setRuedas(int Ruedas);
+    void setHP(int HP);
+    void setRuedaRepuesto(bool RuedaRepuesto);
+    void setKitReparcion(bool KitReparacion);
+    void setColor(string Color);
 
+    //Accesorios
+    void setBurbuja(bool Burbuja);
+    void setPortavasos(bool Portavasos);
+    void setNumeroBocinas(int NumeroBocinas);
+
+    //Matricula
+    void setMatricula(string Matricula);
 
 
 
